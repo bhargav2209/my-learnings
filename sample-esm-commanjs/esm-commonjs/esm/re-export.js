1594b141be5e0aea302cd34bf4642ff5898cc8bb
dineshdabhi <dineshdabhi@cloud.upwork.com>
2024-02-19 19:35:51 +0530

    add re export code syntax

--- sample-esm-commanjs/esm-commonjs/esm/re-export.js
+++ sample-esm-commanjs/esm-commonjs/esm/re-export.js
@@ -1,3 +1,2 @@
-//“Re-export” syntax export ... from ... allows to import things and immediately export them (possibly under another name)
-export func from './json-export.js';
-export defaultFunction from './default-function.js';
+export { default as prop } from './json-export.js';
+export { default as defaultFunction } from './default-function.js';
