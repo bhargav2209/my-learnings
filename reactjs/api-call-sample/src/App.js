4375d681dc7a802c88faca89f3150fb73c976cc4
dineshdabhi <dineshdabhi@cloud.upwork.com>
2024-03-21 17:10:56 +0530

    set the page

--- reactjs/api-call-sample/src/App.js
+++ reactjs/api-call-sample/src/App.js
@@ -1,11 +1,9 @@
 import './App.css';
-import {Button} from "@chakra-ui/react";
 
 function App() {
   return (
     <div className="App">
       <h3>Hello this is state management project </h3>
-      <Button>Checking states</Button>
     </div>
   );
 }
