f93459211e3b297d34308c933af94301c19e4409
dineshdabhi <dineshdabhi@cloud.upwork.com>
2024-03-21 17:35:46 +0530

    set useState on button click

--- reactjs/api-call-sample/src/App.js
+++ reactjs/api-call-sample/src/App.js
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Button } from 'antd';
 import './App.css';
 
 function App() {
-  return (
+    const [data, setData] = useState();
+
+    const handleDataSet = () => {
+       setData('Data Details')
+       console.log(data);
+    }
+
+    return (
     <div className="App">
       <h3>Hello this is state management project </h3>
-      <Button>Checking states</Button>
+      <Button onClick={handleDataSet}>Checking states</Button>
     </div>
-  );
+    );
 }
 
 export default App;
