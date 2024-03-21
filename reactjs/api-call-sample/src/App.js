2146a5854c24a6f96789fb08b1d930272e608312
dineshdabhi <dineshdabhi@cloud.upwork.com>
2024-03-21 17:07:41 +0530

    Move the files

---
+++ reactjs/api-call-sample/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import {Button} from "@chakra-ui/react";
+
+function App() {
+  return (
+    <div className="App">
+      <h3>Hello this is state management project </h3>
+      <Button>Checking states</Button>
+    </div>
+  );
+}
+
+export default App;
