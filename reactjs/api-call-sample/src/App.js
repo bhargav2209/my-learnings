8e6501a65c6bed38edc75cfcb020a8553f6b1788
dineshdabhi <dineshdabhi@cloud.upwork.com>
2024-03-21 17:24:39 +0530

    Add Checking states button

--- reactjs/api-call-sample/src/App.js
+++ reactjs/api-call-sample/src/App.js
@@ -1,9 +1,12 @@
+import React from 'react';
+import { Button } from 'antd';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h3>Hello this is state management project </h3>
+      <Button>Checking states</Button>
     </div>
   );
 }
