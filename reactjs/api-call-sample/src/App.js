e1d3acd680b6536b3770c900af4a81474c358a50
dineshdabhi <dineshdabhi@cloud.upwork.com>
2024-03-21 17:45:07 +0530

    set loading state on button click

--- reactjs/api-call-sample/src/App.js
+++ reactjs/api-call-sample/src/App.js
@@ -3,18 +3,30 @@ import { Button } from 'antd';
 import './App.css';
 
 function App() {
-    const [data, setData] = useState();
+    const [isLoading, setLoading] = useState(false);
 
-    const handleDataSet = () => {
-       setData('Data Details')
-       console.log(data);
-    }
+    const handleClick = () => {
+        setLoading(true);
+        setTimeout(() => {
+            setLoading(false);
+        }, 3000);
+    };
+
+    // const [data, setData] = useState();
+    // const handleDataSet = () => {
+    //    setData('Data Details');
+    //    console.log(data);
+    // }
+
 
     return (
-    <div className="App">
-      <h3>Hello this is state management project </h3>
-      <Button onClick={handleDataSet}>Checking states</Button>
-    </div>
+        <div className="App">
+            <h3>Hello this is state management project </h3>
+            <Button onClick={handleClick} disabled={isLoading}>
+                {isLoading ? 'Loading...' : 'Click me'}
+            </Button>
+            {/*<Button onClick={handleDataSet}>Checking states</Button>*/}
+        </div>
     );
 }
 
