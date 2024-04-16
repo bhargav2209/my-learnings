752ee4ba75e6095e041b18941cd3c17dbbe786df
bhargav2209 <bhargav.raimp@gmail.com>
2024-04-16 11:57:23 +0530

    create basic lambda function

--- aws/cdk-project/src/infra/Launcher.ts
+++ aws/cdk-project/src/infra/Launcher.ts
@@ -1,8 +1,10 @@
 #!/usr/bin/env node
 import { App } from 'aws-cdk-lib';
 import {DataStack} from "./stacks/DataStack";
+import {LambdaStack} from "./stacks/LambdaStack";
 
 const app = new App();
 new DataStack(app, 'DataStack');
+new LambdaStack(app, 'LambdaStack');
 
 
