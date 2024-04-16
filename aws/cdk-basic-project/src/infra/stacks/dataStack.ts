0f362b841f2df2a98669c291846a557cd3a0287b
bhargav2209 <bhargav.raimp@gmail.com>
2024-04-16 11:41:35 +0530

    Remove the cdk-basic-project and create a new project name as a " cdk-project "

--- aws/cdk-basic-project/src/infra/stacks/dataStack.ts
+++
@@ -1,9 +0,0 @@
-import { Stack, StackProps } from "@aws-cdk/core";
-import { Construct } from "constructs"
-
-
-export class DataStack extends Stack{
-    constructor(scope: Construct, id: string, props?: StackProps) {
-        super(scope, id, props);
-    }
-}
---
+++ aws/cdk-project/src/infra/Launcher.ts
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+import { App } from 'aws-cdk-lib';
+import {DataStack} from "./stacks/DataStack";
+
+const app = new App();
+new DataStack(app, 'DataStack');
+
+
