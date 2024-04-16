3b1357defd60e0c6d618a0496b33cdc15c1b053c
bhargav2209 <bhargav.raimp@gmail.com>
2024-04-16 15:05:03 +0530

    create basic table in dynamoDb and deploy it

--- aws/cdk-project/src/infra/Launcher.ts
+++ aws/cdk-project/src/infra/Launcher.ts
@@ -2,9 +2,15 @@
 import { App } from 'aws-cdk-lib';
 import {DataStack} from "./stacks/DataStack";
 import {LambdaStack} from "./stacks/LambdaStack";
+import {ApiStack} from "./stacks/ApiStack";
 
 const app = new App();
-new DataStack(app, 'DataStack');
-new LambdaStack(app, 'LambdaStack');
+const dataStsck = new DataStack(app, 'DataStack');
+const lambdaStack = new LambdaStack(app, 'LambdaStack',{
+    spacesTable: dataStsck.spacesTable
+});
+new ApiStack(app, 'ApiStack', {
+    helloLambdaIntegration: lambdaStack.helloLambdaIntegration
+})
 
 
--- aws/cdk-project/src/infra/stacks/LambdaStack.ts
+++ aws/cdk-project/src/infra/stacks/LambdaStack.ts
@@ -2,14 +2,30 @@ import {Stack, StackProps} from "aws-cdk-lib";
 import {Construct} from "constructs";
 import {Code, Runtime, Function as LambdaFunction} from "aws-cdk-lib/aws-lambda";
 import { join } from "path";
+import {LambdaIntegration} from "aws-cdk-lib/aws-apigateway";
+import {ITable} from "aws-cdk-lib/aws-dynamodb";
+
+interface LambdaStackProps extends StackProps{
+    spacesTable: ITable
+}
+
 export class LambdaStack extends Stack{
-    constructor(scope: Construct, id: string, props?: StackProps) {
+
+    public readonly helloLambdaIntegration: LambdaIntegration
+
+    constructor(scope: Construct, id: string, props: LambdaStackProps) {
         super(scope, id, props)
 
-        new LambdaFunction(this, 'HelloLambda', {
+        const helloLambda = new LambdaFunction(this, 'HelloLambda', {
             runtime: Runtime.NODEJS_16_X,
             handler: 'hello.main',
-            code: Code.fromAsset(join(__dirname, '..', '..', 'services'))
+            code: Code.fromAsset(join(__dirname, '..', '..', 'services')),
+            environment: {
+                TABLE_NAME: props.spacesTable.tableName
+            }
         })
+
+        this.helloLambdaIntegration = new LambdaIntegration(helloLambda);
+
     }
 }
