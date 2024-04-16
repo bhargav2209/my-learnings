39c97eb21f693021792e99eaa692791cdea9d94d
bhargav2209 <bhargav.raimp@gmail.com>
2024-04-16 15:55:11 +0530

    create nodejs lambda function and deploy it

--- aws/cdk-project/src/infra/stacks/LambdaStack.ts
+++ aws/cdk-project/src/infra/stacks/LambdaStack.ts
@@ -1,9 +1,10 @@
-import {Stack, StackProps} from "aws-cdk-lib";
-import {Construct} from "constructs";
-import {Code, Runtime, Function as LambdaFunction} from "aws-cdk-lib/aws-lambda";
+import { Stack, StackProps } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import { Runtime } from "aws-cdk-lib/aws-lambda";
 import { join } from "path";
-import {LambdaIntegration} from "aws-cdk-lib/aws-apigateway";
-import {ITable} from "aws-cdk-lib/aws-dynamodb";
+import { LambdaIntegration } from "aws-cdk-lib/aws-apigateway";
+import { ITable } from "aws-cdk-lib/aws-dynamodb";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
 
 interface LambdaStackProps extends StackProps{
     spacesTable: ITable
@@ -16,10 +17,10 @@ export class LambdaStack extends Stack{
     constructor(scope: Construct, id: string, props: LambdaStackProps) {
         super(scope, id, props)
 
-        const helloLambda = new LambdaFunction(this, 'HelloLambda', {
+        const helloLambda = new NodejsFunction(this, 'HelloLambda', {
             runtime: Runtime.NODEJS_16_X,
-            handler: 'hello.main',
-            code: Code.fromAsset(join(__dirname, '..', '..', 'services')),
+            handler: 'handler',
+            entry: (join(__dirname, '..', '..', 'services', 'hello.ts')),
             environment: {
                 TABLE_NAME: props.spacesTable.tableName
             }
